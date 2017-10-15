0fbadd108ccec647ff2d45fafca33b6cc8208d59
D <drewpcchannel@gmail.com>
2017-10-14 22:29:44 -0600

    camera is now top down

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,11 +8,11 @@ public class CameraFollow : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        transform.rotation = Quaternion.Euler(75, 0, 0);
+        transform.rotation = Quaternion.Euler(90, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector3(player.position.x, 10, player.position.z);
+        transform.position = new Vector3(player.position.x, 15, player.position.z);
 	}
 }
