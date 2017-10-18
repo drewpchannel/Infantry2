868be59235675cc2b294d909e5bb21ae53fc4661
D <drewpcchannel@gmail.com>
2017-10-18 14:03:51 -0600

    enemy damages player

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour {
+
+    public int health;
+    public int currentHealth;
+
+    // Use this for initialization
+    void Start () {
+        currentHealth = health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentHealth <= 0 )
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    public void HurtPlayer (int damage)
+    {
+        currentHealth -= damage;
+    }
+}
